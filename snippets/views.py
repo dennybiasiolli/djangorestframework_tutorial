93e252cac4adc94147aa86f7925b4f3c635153c0
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2016-12-07 11:41:01 +0100

    creating an endpoint for the highlighted snippets

--- snippets/views.py
+++ snippets/views.py
@@ -4,7 +4,7 @@ from snippets.permissions import IsOwnerOrReadOnly
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, renderers
 from django.contrib.auth.models import User
 
 
@@ -30,6 +30,16 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = SnippetSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,
                           IsOwnerOrReadOnly,)
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = (renderers.StaticHTMLRenderer,)
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
