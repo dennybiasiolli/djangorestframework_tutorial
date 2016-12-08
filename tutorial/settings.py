469b81363ac1a809b2c61e5362cef6c226b36e1d
Denny Biasiolli <denny.biasiolli@gmail.com>
2016-12-08 20:19:55 +0100

    adding rest_framework.authtoken to INSTALLED_APPS
    Note: Make sure to run `manage.py migrate` after changing your settings.
    The `rest_framework.authtoken` app provides Django database migrations.

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'snippets.apps.SnippetsConfig',
 ]
 
