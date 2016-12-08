d5a55162292fb5d5c3377a8d941783290cecac25
Denny Biasiolli <denny.biasiolli@gmail.com>
2016-12-08 20:30:05 +0100

    adding api-token-auth to api urls to obtain auth token

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import url, include
-from snippets import views
+from snippets.views import SnippetViewSet
 from rest_framework.routers import DefaultRouter
 from rest_framework.schemas import get_schema_view
+from rest_framework.authtoken import views
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
-router.register(r'snippets', views.SnippetViewSet)
-router.register(r'users', views.UserViewSet)
+router.register(r'snippets', SnippetViewSet)
 
 schema_view = get_schema_view(title='Pastebin API')
 
@@ -15,5 +15,6 @@ schema_view = get_schema_view(title='Pastebin API')
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api-token-auth/', views.obtain_auth_token),
     url('^schema/$', schema_view),
 ]
